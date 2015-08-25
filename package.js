d1ae25770da30b20d8e28afcf9b474b277c39da7
Zhenya <zhenya@meteor.com>
2015-08-24 22:08:34 -0700

    version bump

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
 	name: 'zhenya:reveal',
-	version: '0.0.3',
+	version: '0.0.4',
 	// Brief, one-line summary of the package.
 	summary: 'Animate data context changes',
 	// URL to the Git repository containing the source code for this package.
